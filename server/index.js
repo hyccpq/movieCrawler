61d782ae723454c9c3f3bccb4440de489d24ffda
hyccpq <hyccpq@hotmail.com>
2018-03-31 12:20:33 +0800

    testRouter

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const Koa = require('koa')
 const mongooose = require('mongoose')
 const { connect, initSchemas } = require('./database/init')
+const routes = require('./routes')
 
+const app = new Koa()
 ;(async () =>{
 	await connect()
 	
@@ -9,5 +11,13 @@ const { connect, initSchemas } = require('./database/init')
 	// await require('./tasks/process')
 	// await require('./tasks/api')
 	// await require('./tasks/trailer_video_process')
-	await require('./tasks/qiniu')
+	// await require('./tasks/qiniu')
 })()
+
+app
+	.use(routes.routes())
+	.use(routes.allowedMethods())
+
+app.listen(5678, () => {
+	console.log('服务运行于http://localhost:5678');
+})
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,2 +1,33 @@
 const Router = require('koa-router')
-const Movie = mongoose.model('Movie')
+const mongoose = require('mongoose')
+
+const router = new Router()
+
+router.get('/movies/all', async (ctx, next) => {
+	const Movie = mongoose.model('Movie')
+	try{
+		const movies = await Movie.find().sort({
+			'meta.createdAt' : -1
+		})
+		ctx.body = {
+			movies
+		}
+	} catch (e) {
+		console.log(e);
+	}
+})
+
+router.get('/movies/detail/:id', async (ctx, next) => {
+	const Movie = mongoose.model('Movie')
+	const id = ctx.params.id
+	try{
+		const movies = await Movie.findOne({_id : id})
+		ctx.body = {
+			movies
+		}
+	} catch (e) {
+		console.log(e);
+	}
+})
+
+module.exports = router
