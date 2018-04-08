59058f93c838608a3c51667e7ffc8c903c6a44e6
hyccpq <hyccpq@hotmail.com>
2018-04-09 00:23:29 +0800

    webpackDev1

---
+++ front/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <input type="text" v-model="sth">{{ sth }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: "App",
+    data(){
+        return {
+            sth:''
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- front/src/main.js
+++ front/src/main.js
@@ -1,1 +1,7 @@
-console.log('66666')
+import Vue from 'vue'
+import App from './App.vue'
+
+new Vue({
+	el:'#app',
+	render: h => h(App),
+})
